b7fe66f02b1ac65e51ce20f04f7887cdc688e483
VaishnaviiMishra <146117422+VaishnaviiMishra@users.noreply.github.com>
2024-08-16 11:47:56 +0530

    yes

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,19 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
 /**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
+ * Metro configuration for React Native
+ * https://github.com/facebook/metro
+ * @format
  */
-const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+  },
+};
